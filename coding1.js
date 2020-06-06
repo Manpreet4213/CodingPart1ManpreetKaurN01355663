109260989bab1e1bac9a7e88d021963cd7a8e1f5
Manpreet Kaur <manpreetkaur5717@gmail.com>
2020-06-05 20:23:20 -0500

    data fetched

--- coding1.js
+++ coding1.js
@@ -3,16 +3,16 @@ let employee_list = [];
 function onload() {
     
     getMyEmployees()
-    .then(ouyput => {
+    .then(output => {
         console.log(output);
-        for(let a in output) {
-            let photo = data[a].employeeshasphoto ==="1" ? '<img style="width: 95%; border-radius=50%; padding-top = 2px; "
-        }
-    }
+        //for(let a in output){
+           // let photo = data[a].employeeshasphoto ==="1" ? '<img style="width: 95%; //border-radius=50%; padding-top = 2px;"
+        //}
+    });
 }
 
 async function getMyEmployees() {
-    let url = await fetch("http://sandbox.bittsdevelopment.com/code1/fetchemployees.php", {
+    let response = await fetch("http://sandbox.bittsdevelopment.com/code1/fetchemployees.php", {
        method : "GET", 
     });
     let output = await response.json();
