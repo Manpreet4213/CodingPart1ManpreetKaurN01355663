36e28a46f53f4f7bd4836d3482a8efc2144c1a52
Manpreet Kaur <manpreetkaur5717@gmail.com>
2020-06-05 19:26:08 -0500

    initial commit

---
+++ coding1.js
@@ -0,0 +1,22 @@
+let employee_list = [];
+
+function onload() {
+    
+    getMyEmployees()
+    .then(ouyput => {
+        console.log(output);
+        for(let a in output) {
+            let photo = data[a].employeeshasphoto ==="1" ? '<img style="width: 95%; border-radius=50%; padding-top = 2px; "
+        }
+    }
+}
+
+async function getMyEmployees() {
+    let url = await fetch("http://sandbox.bittsdevelopment.com/code1/fetchemployees.php", {
+       method : "GET", 
+    });
+    let output = await response.json();
+    return output;
+}
+
+window.addEventListener('load', onload);
