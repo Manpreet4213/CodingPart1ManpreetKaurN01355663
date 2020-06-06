2beb03824c14af0fa72c61f30431c02c7f95d112
Manpreet Kaur <manpreetkaur5717@gmail.com>
2020-06-05 23:42:42 -0500

    tried fetching image

--- coding1.js
+++ coding1.js
@@ -14,7 +14,43 @@ function onload() {
         //let featured = output[a].employeeisfeatured === "1" ? `<div class="crown"> 👑</div>` : ``;
         // i have copied this crown image from the link https://hotemoji.com/crown-emoji.html
         // the crown thing is also not appearing for me.
-        }
+        
+        
+        //loadPhoto('sandbox.bittsdevelopment.com/code1/employeepics/2.jpg')
+        //.then(console.log)
+        //.catch(console.error);
+
+
+
+
+
+        // I tried below code from the link https://blog.logrocket.com/improve-async-programming-with-javascript-promises-1652ac8d036d/ but it is printing the image three times i cannot understand why?
+        //
+       const loadPhoto = photo_url => new Promise((resolve, reject) => {
+      // Create a new Image object
+      const img = new Image();
+
+      // Set the image source
+      img.src = photo_url;
+
+      // If the image is loads,
+      // fulfil the promise with the Image object
+      img.onload = () => resolve(img);
+
+      // If there was an error,
+      // reject the promise with the Error object
+      img.onerror = evt => reject(new Error('Image could not be loaded.'));
+    });
+
+    loadPhoto('http://sandbox.bittsdevelopment.com/code1/employeepics/3.jpg')
+      .then(img => {
+        img.alt = 'image of an employee';
+        
+        console.log(img);
+        document.body.appendChild(img);
+      })
+      .catch(console.error);
+            }
     });
 }
 
